69f4db0ec5f941b548bc18df5649f35e96506ed1
n-kaya55 <nesibe.kaya104@gmail.com>
2020-11-23 14:31:01 +0300

    ucuncu adim

--- src/GitTekrar.java
+++ src/GitTekrar.java
@@ -3,7 +3,8 @@ public class GitTekrar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("birinci adým");
+		System.out.println("birinci adim");
+		System.out.println("ikinci adim");
 		
 		
 		
